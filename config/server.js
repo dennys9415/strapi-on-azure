57ed56d8a1d958ecc6615b31e59a93a1bc773c5e
root <root@Dennys.localdomain>
2021-12-06 13:42:21 -0500

    third test

--- config/server.js
+++ config/server.js
@@ -1,4 +1,11 @@
+// path: ./config/env/production/server.js
+
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  url: "https://strapi-on-azure-dennys.azurewebsites.net",
+  admin: {
+    url: "https://proud-ground-0eb09500f.azurestaticapps.net",
+    serveAdminPanel: false,
+  },
 });
