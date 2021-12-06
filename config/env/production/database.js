23296e93fbe3249fb74362b7b58a0e96f9c61944
root <root@Dennys.localdomain>
2021-12-06 13:20:14 -0500

    test 2

---
+++ config/env/production/database.js
@@ -0,0 +1,20 @@
+// path: ./config/env/production/database.js
+
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'postgres',
+        host: env('DATABASE_HOST'),
+        port: env.int('DATABASE_PORT'),
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
+        ssl: env.bool('DATABASE_SSL'),
+      },
+      options: {}
+    },
+  },
+});
